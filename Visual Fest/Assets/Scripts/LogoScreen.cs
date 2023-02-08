4c71dca24b796684b12afd49ee6493538a4d9296
REAPER073 <alimuhammedashraf1@gmail.com>
2023-02-08 15:57:53 +0500

    Added Unity Project
    Added the v0.1 version of the App.

---
+++ Visual Fest/Assets/Scripts/LogoScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoScreen : MonoBehaviour
+{
+    public GameObject logoscreen;
+    Image bgimg;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}

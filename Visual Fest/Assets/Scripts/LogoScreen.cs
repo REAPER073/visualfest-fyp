71c4757467f059995d9314dc5dbb3d8f09148711
REAPER073 <alimuhammedashraf1@gmail.com>
2023-02-08 17:07:45 +0500

    Added Logo Screen animation Script

--- Visual Fest/Assets/Scripts/LogoScreen.cs
+++ Visual Fest/Assets/Scripts/LogoScreen.cs
@@ -6,25 +6,43 @@ using UnityEngine.UI;
 public class LogoScreen : MonoBehaviour
 {
     public Image logobg;
-    public float alphaValue = 0.5f;
+    public float minAlphaValue = 0.0f;
+    public float maxAlphaValue = 1.0f;
+    public float speed = 1.0f;
+    private bool increasing = true;
     // Start is called before the first frame update
     void Start()
     {
-        if (logobg == null)
-        {
-            Debug.LogError("Image component not assigned!");
-            return;
-        }
-
-        Color tempColor = logobg.color;
-        tempColor.a = alphaValue;
-        logobg.color = tempColor;
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        Color tempColor = logobg.color;
+        float currentAlpha = tempColor.a;
+
+        if (increasing)
+        {
+            currentAlpha += Time.deltaTime * speed;
+            if (currentAlpha >= maxAlphaValue)
+            {
+                increasing = false;
+                currentAlpha = maxAlphaValue;
+            }
+        }
+        else
+        {
+            currentAlpha -= Time.deltaTime * speed;
+            if (currentAlpha <= minAlphaValue)
+            {
+                increasing = true;
+                currentAlpha = minAlphaValue;
+            }
+        }
+
+        tempColor.a = currentAlpha;
+        logobg.color = tempColor;
     }
+
 }
